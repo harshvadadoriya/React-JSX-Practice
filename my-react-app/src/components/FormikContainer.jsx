227381a49c2682167734845d59cc2f1cb21eded1
Harsh Vadadoriya <harshvadadoriya079@gmail.com>
2023-05-14 12:24:47 +0530

    added radio, select and checkbox group

--- my-react-app/src/components/FormikContainer.jsx
+++ my-react-app/src/components/FormikContainer.jsx
@@ -4,13 +4,36 @@ import * as Yup from 'yup';
 import FormikControl from './FormikControl';
 
 const FormikContainer = () => {
+	const dropDownOptions = [
+		{ key: 'Select an option', value: '' },
+		{ key: 'Option 1', value: 'option1' },
+		{ key: 'Option 2', value: 'option2' },
+		{ key: 'Option 3', value: 'option3' },
+	];
+	const radioOptions = [
+		{ key: 'Option 1', value: 'rOption1' },
+		{ key: 'Option 2', value: 'rOption2' },
+		{ key: 'Option 3', value: 'rOption3' },
+	];
+	const checkboxOptions = [
+		{ key: 'Option 1', value: 'cOption1' },
+		{ key: 'Option 2', value: 'cOption2' },
+		{ key: 'Option 3', value: 'cOption3' },
+	];
+
 	const initialValue = {
 		email: '',
 		description: '',
+		selectOption: '',
+		radioOption: '',
+		checkboxOption: [],
 	};
 	const validationSchema = Yup.object({
 		email: Yup.string().required('Required'),
 		description: Yup.string().required('Required'),
+		selectOption: Yup.string().required('Required'),
+		radioOption: Yup.string().required('Required'),
+		checkboxOption: Yup.array().min(1, 'Required'),
 	});
 	const onSubmit = (values) => console.log('Form data', values);
 	return (
@@ -31,6 +54,24 @@ const FormikContainer = () => {
 						control="textarea"
 						label="Description"
 						name="description"
+					/>
+					<FormikControl
+						control="select"
+						label={'select a Topic'}
+						name="selectOption"
+						options={dropDownOptions}
+					/>
+					<FormikControl
+						control="radio"
+						label="Radio Topic"
+						name="radioOption"
+						options={radioOptions}
+					/>
+					<FormikControl
+						control="checkbox"
+						label="Checkbox topics"
+						name="checkboxOption"
+						options={checkboxOptions}
 					/>
 					<button type="submit">Submit</button>
 				</Form>
--- my-react-app/src/components/FormikControl.jsx
+++ my-react-app/src/components/FormikControl.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import Input from './Input';
 import TextArea from './TextArea';
+import Select from './Select';
+import Radio from './Radio';
+import CheckboxGroup from './CheckboxGroup';
 
 const FormikControl = (props) => {
 	const { control, ...rest } = props;
@@ -10,8 +13,11 @@ const FormikControl = (props) => {
 		case 'textarea':
 			return <TextArea {...rest} />;
 		case 'select':
+			return <Select {...rest} />;
 		case 'radio':
+			return <Radio {...rest} />;
 		case 'checkbox':
+			return <CheckboxGroup {...rest} />;
 		case 'date':
 		default:
 			return null;
